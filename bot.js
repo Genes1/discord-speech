5c257d2fd6f23819c1c32640bdf744984cdda916
Domrachev <eug.domr@gmail.com>
2020-07-24 23:28:23 -0400

    Stuck on audio recording

--- bot.js
+++ bot.js
@@ -1,11 +1,17 @@
 const path = require("path")
 const Discord = require('discord.js');
 const tokenFile = require(path.join(__dirname, "./config/token.json")); 
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 
 const client = new Discord.Client();
 
 
+ffmpeg.setFfmpegPath(path.resolve(__dirname, 'node_modules', '.bin', 'ffmpeg'));
+ffmpeg.setFfprobePath(
+  path.resolve(__dirname, 'node_modules', '.bin', 'ffprobe')
+);
 
 
 client.on('ready', () => {
@@ -27,14 +33,60 @@ client.on('message', async message => {
 
 
   if (message.content === '.join') {
+
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
+    	const connection = await message.member.voice.channel.join();
+    	message.reply(message.member);
+    	const receiver = await connection.receiver.createStream(message.member, { mode: 'opus' });
+    	receiver.resume();
+    	console.log(receiver);
+    	console.log(receiver.isPaused());
+    	receiver.on('data', (chunk) => {
+    		console.log(`Received ${chunk.length} bytes of data.`);
+    	});
+
+    	receiver.on('end', () => {
+    		console.log('There will be no more data.');
+    	});
+
+    	receiver.pipe(fs.createWriteStream('user_audio'));
+
+
+/*    	try {
+    		const out = fs.createWriteStream('./audio.wav');
+    		ffmpeg(receiver)
+    		.inputFormat('s32le')
+    		.audioFrequency(16000)
+    		.audioChannels(1)
+    		.audioCodec('pcm_s16le')
+    		.format('s16le')
+    		.on('error', console.error.bind(console))
+    		.pipe(out);
+    	} catch (error) {
+    		console.log(error);
+    	}
+*/
+
     } else {
-      message.reply('You need to join a voice channel first!');
+    	message.reply('You need to join a voice channel first!');
     }
-  }
+
+
+    
+
+
+
+}
+
+
 });
+
+
+
+
+
+// TODO make the bot listen to all users in the channel
 
 
 client.on("error", console.error);
